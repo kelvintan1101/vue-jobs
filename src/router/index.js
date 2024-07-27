edb14ba9a867e492a234806fd31f0a2cc92dd954
Kelvin <kelvintan1101@gmail.com>
2024-07-27 18:00:08 +0800

    refactor: Add EditJobView component for editing job details
    The code changes in `index.js` add the `EditJobView` component to handle editing job details. This new component allows users to update job information, improving the functionality and user experience of the application.

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,8 @@ import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import JobView from "@/views/JobView.vue";
-import AddJobView from "../views/AddJobView.vue";
+import AddJobView from "@/views/AddJobView.vue";
+import EditJobView from "@/views/EditJobView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +28,11 @@ const router = createRouter({
       path: "/jobs/add",
       name: "add-job",
       component: AddJobView,
+    },
+    {
+      path: "/jobs/edit/:id",
+      name: "edit-job",
+      component: EditJobView,
     },
     {
       path: "/:catchAll(.*)",
